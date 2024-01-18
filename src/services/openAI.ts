3a769a1e7b030e4b559aba532a628dc636ff3d88
Rajneshwar Singh <rajneshwarthakur@gmail.com>
2024-01-19 02:23:45 +0530

    Initial commit

--- src/services/openAI.ts
+++ src/services/openAI.ts
@@ -3,13 +3,11 @@ const openai = new OpenAI({ apiKey: process.env.OpenAI_API_KEY });
 
 export class OpenAIServices {
   public static async generateText(prompt: unknown) {
-    console.log('process.env.OpenAI_API_KEY', process.env.OpenAI_API_KEY);
-    return {};
-    // const completion = await openai.chat.completions.create({
-    //   model: 'gpt-3.5-turbo',
-    //   messages: [{ role: "user", content: prompt + ". Provide your answer in maximum 2000 characters using bullet points. Also don't mention this 2000 character limit in your response" }],
-    //   max_tokens: 1000,
-    // });
-    // return completion?.choices[0]
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: "user", content: prompt + ". Provide your answer in maximum 2000 characters using bullet points. Also don't mention this 2000 character limit in your response" }],
+      max_tokens: 1000,
+    });
+    return completion?.choices[0]
   }
 }
